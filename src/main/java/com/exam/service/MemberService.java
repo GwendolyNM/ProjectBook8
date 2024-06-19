33fcde28c77872bd35e91fb133f903e927c53aa1
CarrotGold <chunius@naver.com>
2024-06-19 18:07:03 +0900

    HJG_test

--- src/main/java/com/exam/service/MemberService.java
+++ src/main/java/com/exam/service/MemberService.java
@@ -7,6 +7,7 @@ import com.exam.dto.MemberDTO;
 
 public interface MemberService {
 
+	
 	public int save(MemberDTO member_id);
 	public MemberDTO findById(String member_id);
 	public List<MemberDTO> memberList();
